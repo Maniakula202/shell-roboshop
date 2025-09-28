f2fe9254a45fb10cd3d3a368f0f75657c0b6c4bc
Manikanta Akula <maniakula202@gmail.com>
2025-09-28 11:52:13 +0530

    shell practice

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ DOMAIN_NAME="manidevops.fun"
 
 for instance in $@
 do 
-    INSTANCE_ID=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type t3.micro --security-group-ids sg-0d7d38fd2dd9739a0 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=$(aws ec2 run-instances --image-id $IMAGE_ID --instance-type t3.micro --security-group-ids $SG --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
 
     if [ $instance != "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
